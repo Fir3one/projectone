c221f99b29939129b27edbc8295c117e8a5f7051
Christoforos <mxristoforos21@gmail.com>
2023-03-21 21:41:46 +0100

    styling

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const path = require("path")
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"))
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
     res.render("home")
 });
 
